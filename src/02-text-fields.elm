63c389cbcf021be50e4726cdfa3ea1c7239b2ce5
fanie <faniepotgieter@gmail.com>
2018-09-17 18:51:33 +0200

    add example for 02-text-fields

---
+++ src/02-text-fields.elm
@@ -0,0 +1,59 @@
+module Main exposing (..)
+
+import Browser
+import Html exposing (Html, div, input, p, text)
+import Html.Attributes exposing (value, placeholder)
+import Html.Events exposing (onInput)
+
+
+-- MAIN
+
+
+main =
+    Browser.sandbox
+        { init = init
+        , update = update
+        , view = view
+        }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    { content : String
+    }
+
+
+init : Model
+init =
+    { content = ""
+    }
+
+
+
+-- UPDATE
+
+
+type Msg
+    = Change String
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Change newContent ->
+            { model | content = newContent }
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ input [ placeholder "Text to reverse", value model.content, onInput Change ] []
+        , p [] [ text (String.reverse model.content) ]
+        ]
